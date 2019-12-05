44292ddb43c3b51e9b94f56febfaad1e9c642647
Joseph Lao <n01370825@nj-00118-017.humber.org>
2019-12-05 15:56:43 -0500

    added add to cart functionality and checkout section

--- js/main.js
+++ js/main.js
@@ -5,13 +5,24 @@ const SETTINGS = {
     imagePath: 'img/products/'
 };
 
+// start with empty cart
+const CART = {
+    items: [],
+    add: function (item) { this.items.push(item) },
+    remove: function (index) { this.items.splice(index, 1) },
+}
+
 /**
  * get products data using async await
  */
 const getProductsData = async () => await fetch(`data/products.json`).then(d => d.json()).catch(e => console.error(e));
 
+/**
+ * returns product as html template
+ * @param {{}} product 
+ */
 const getProductTemplate = product => {
-    const getRatingsHTML = rating => {
+    const getRatingsHTML = rating => { // takes rating number and converts it to material icon
         let halfStar = rating % 1;
         let starIcon = '';
         for (let i = 0; i < 5; i++) {
@@ -53,6 +64,10 @@ const getProductTemplate = product => {
     </section>`
 };
 
+/**
+ * renders products on html page
+ * @param {{}} products 
+ */
 const renderProductsOnHTML = products =>
     document.getElementById(`products`).innerHTML =
     `<p>Showing ${products.length} products...</p>` + products.reduce((acc, p) => acc + getProductTemplate(p), ``);
@@ -71,7 +86,7 @@ const getSortedProducts = (sortby, products) => (sortby.startsWith('p')) ?
  * @param {string} search 
  * @param {{}} products 
  */
-const getSearchedProducts = (search, products) => products.filter(p => p.name.toLowerCase().includes(search.trim()));
+const getSearchedProducts = (search, products) => products.filter(p => p.name.toLowerCase().includes(search));
 
 /**
  * returns products by specific category
@@ -85,13 +100,35 @@ const getProductsByCategory = (category, products) => products.filter(p => p.cat
  * @param {{}} products 
  */
 const doFiltering = products => {
-    const category = document.getElementById(`category`).value
-    const sort = document.getElementById(`sort`).value
-    const search = document.getElementById(`search`).value
+    const category = document.getElementById(`category`).value;
+    const sort = document.getElementById(`sort`).value;
+    const search = document.getElementById(`search`).value.trim();
     // filter products by category then sort accordingly if search value is > 2 then search product
     const filteredProducts = getSearchedProducts(search.length > 2 ? search : '', getSortedProducts(sort, !category ? products : getProductsByCategory(category, products)));
     renderProductsOnHTML(filteredProducts)
 };
+
+/**
+ * add selected product to cart
+ * @param {string} productId 
+ */
+const addProductToCart = productId => {
+    const caseSize = +document.querySelector(`input[name="${productId}_caseSize"]:checked`).value;
+
+    // find product on cart if it exist increment qty else add item to cart
+    const cartItem = CART.items.find(item => item.id == productId && item.size == caseSize);
+    if (cartItem) cartItem.quantity++;
+    else CART.add({ id: productId, size: caseSize, quantity: 1 });
+
+    document.getElementById(`cart_count`).innerText = CART.items.length;
+};
+
+/**
+ * toggles cart section show/hide
+ */
+const toggleShoppingCart = () => {
+    document.querySelector(`aside.cart.wrapper`).classList.toggle(`show`);
+}
 
 /**
  * window on load event listener
@@ -107,5 +144,14 @@ window.addEventListener(`load`, async () => {
     // add search event listener
     document.getElementById(`search`).addEventListener(`input`, listenToFilterEvents);
     // add filter event listener for sort and category
-    document.querySelectorAll(`select`).forEach(s => s.addEventListener(`change`, listenToFilterEvents))
+    document.querySelectorAll(`select`).forEach(s => s.addEventListener(`change`, listenToFilterEvents));
+
+    // body click event deligation
+    document.body.addEventListener(`click`, ({ target }) => {
+        const match = str => target.matches(str);
+        if (match(`button[data-productid]`)) addProductToCart(target.dataset.productid);
+        else if(match(`.cart.wrapper`) || match(`button.btn.shopping_cart`) || match(`button.btn.shopping_cart > .material-icons`)) toggleShoppingCart();
+        else return;
+    });
+
 });
